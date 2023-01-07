bea3c7fc9827813e7b72392f98b2a95811b9d815
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-01-07 21:32:58 +0700

    save date nodejs

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const myContacts = require('./contact'); 
+
+// Menyimpan Data
+const qu = async () => {
+     const name = await datContact.writeQuestion('Masukkan nama anda : ');
+     const email = await datContact.writeQuestion('Masukkan email anda : ');
+     const phoneNumber = await datContact.writeQuestion('Masukkan Nomor HP anda : ');
+
+     datContact.saveContact(name, email, phoneNumber);
+}
+
+qu();
