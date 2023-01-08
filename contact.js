3dfab127f071f2dd21a3351f0ea650ee5fb291cf
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-01-08 13:18:49 +0700

    check name

--- contact.js
+++ contact.js
@@ -1,13 +1,13 @@
 // File System
-const { rejects } = require('assert');
 const fs = require('fs');
-const resolve = require('path');
+const chalk = require('chalk');
+const validator = require('validator');
 
-const readline = require('readline');
-const rl = readline.createInterface({
-     input: process.stdin,
-     output: process.stdout,
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//      input: process.stdin,
+//      output: process.stdout,
+// });
 
 // Cek folder data
 // Jika belum ada maka akan dibuatkan folder baru
@@ -20,13 +20,13 @@ const datPath = './data/contacts.json';
 !fs.existsSync(datPath) ? fs.writeFileSync(datPath,'[]','utf-8') : '';
 
 // Pertanyaan
-const writeQuestion = (quest) => {
-     return new Promise((resolve, rejects) => {
-          rl.question(quest, (name) => {
-               resolve(name);
-          });
-     });
-};
+// const writeQuestion = (quest) => {
+//      return new Promise((resolve, rejects) => {
+//           rl.question(quest, (name) => {
+//                resolve(name);
+//           });
+//      });
+// };
 
 // Simpan
 const saveContact = (name, email, phoneNumber) => {
@@ -34,13 +34,20 @@ const saveContact = (name, email, phoneNumber) => {
      const myFile = fs.readFileSync('data/contacts.json','utf-8');
      const datContact = JSON.parse(myFile);
 
-     datContact.push(contact);
+     // datContact.push(contact);
 
-     fs.writeFileSync('data/contacts.json', JSON.stringify(datContact, null, 2));
+     // fs.writeFileSync('data/contacts.json', JSON.stringify(datContact, null, 2));
 
-     console.log('Terima kasih sudah memasukkan data..');
+     // console.log('Terima kasih sudah memasukkan data..');
 
-     rl.close();
+     // rl.close();
+
+     // Cek nama tidak boleh sama
+     const duplicate = datContact.find((contact) => contact.name === name);
+     if(duplicate) {
+          console.log(chalk.pink.inverse.bold('Kontak sudah terdaftar, Gunakan nama lain!'));
+          return false;
+     }
 }
 
 module.exports = { writeQuestion, saveContact };
