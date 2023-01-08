7c1f436ca075a7f4e392459bdb72abe7da972766
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-01-08 13:09:35 +0700

    argument app js

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ const yargs = require('yargs');
 const datContact = require('./contact'); 
 
 // Menyimpan Data
-const qu = async () => {
-     const name = await datContact.writeQuestion('Masukkan nama anda : ');
-     const email = await datContact.writeQuestion('Masukkan email anda : ');
-     const phoneNumber = await datContact.writeQuestion('Masukkan Nomor HP anda : ');
+// const qu = async () => {
+//      const name = await datContact.writeQuestion('Masukkan nama anda : ');
+//      const email = await datContact.writeQuestion('Masukkan email anda : ');
+//      const phoneNumber = await datContact.writeQuestion('Masukkan Nomor HP anda : ');
 
-     datContact.saveContact(name, email, phoneNumber);
-}
+//      datContact.saveContact(name, email, phoneNumber);
+// }
 
-qu();
+// qu();
+
+yargs.command({
+     commad: 'add',
+     describe: 'Menambahkan kontak baru',
+     builder: {
+          name: {
+               describe: 'Nama lengkap',
+               demandOption: true,
+               type: 'string',
+          },
+          email: {
+               describe: 'Email',
+               demandOption: false,
+               type: 'string',
+          },
+          phoneNumber: {
+               describe: 'Nomor Handphone',
+               demandOption: true,
+               type: 'string',
+          },
+     },
+     handler(argv) {
+          datContact.saveContact(argv.name, argv.email, argv.phoneNumber);
+     },
+     
+});
+
+yargs.parse();
